a94f16c9bb36355893111f3d08ff2d215fe2d5e5
Qurat-ul-ain <145261717+Qurat-ul-ainnn@users.noreply.github.com>
2026-01-02 14:52:58 +0500

    Create stored procedure to load data into bronze schema
    This stored procedure loads data into the 'bronze' schema from external CSV files, truncating tables before loading.

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,97 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
+TRUNCATE TABLE bronze.crm_cust_info;
+
+BULK INSERT bronze.crm_cust_info
+FROM 'E:\DWP DATASET\source_crm\cust_info.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+
+
+TRUNCATE TABLE bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'E:\DWP DATASET\source_crm\prd_info.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'E:\DWP DATASET\source_crm\sales_details.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+
+TRUNCATE TABLE bronze.crm_cust_info;
+
+BULK INSERT bronze.crm_cust_info
+FROM 'E:\DWP DATASET\source_crm\cust_info.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+BULK INSERT bronze.erp_loc_a101
+FROM 'E:\DWP DATASET\source_erp\loc_a101.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'E:\DWP DATASET\source_erp\cust_az12.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'E:\DWP DATASET\source_erp\px_cat_g1v2.csv'
+WITH(
+FIRSTROW = 2,
+FIELDTERMINATOR = ',',
+TABLOCK
+
+);
+
+
